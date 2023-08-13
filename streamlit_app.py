99b35d1751c21d49a359b04f38a8a45c028120cd
marianyzacaroni <marianyzacaroni22@gmail.com>
2023-08-13 00:13:25 -0300

    primeiro commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,11 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
+p = st.chat_input()
+
+if p:
+    with st.chat_message("user"): 
+        st.write(p)
 
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
